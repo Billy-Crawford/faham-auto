d17d02b8d4a55f1a199ebdaf0661464ff1501245
Billy-Crawford <oumaroubilly6@gmail.com>
2026-03-24 00:11:41 +0000

    profil responsive

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,22 +1,31 @@
 import PageHeader from "@/components/PageHeader";
-import ProfileForm from "../../components/ProfileForm";
-import KYCUpload from "../../components/KYCUpload";
+import ProfileForm from "@/components/ProfileForm";
+import KYCUpload from "@/components/KYCUpload";
 import AuthGuard from "@/components/AuthGuard";
 
 export default function ProfilePage() {
   return (
     <AuthGuard>
-      <main className="px-6 md:px-10 py-10 space-y-10">
+      <main className="px-4 sm:px-6 md:px-10 py-6 sm:py-10 space-y-6 sm:space-y-10">
+        
         <PageHeader title="Mon profil" />
 
-        <div className="grid lg:grid-cols-2 gap-10">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 sm:gap-10">
+          
           {/* Infos utilisateur */}
-          <ProfileForm />
+          <div className="order-1 lg:order-none">
+            <ProfileForm />
+          </div>
 
           {/* KYC */}
-          <KYCUpload />
+          <div className="order-2 lg:order-none">
+            <KYCUpload />
+          </div>
+
         </div>
+
       </main>
     </AuthGuard>
   );
 }
+
